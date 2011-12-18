3a2f986f43a405f9558aa674ecc204d7b99721a9
Evan Whalen <evanwhalen@gmail.com>
2011-12-18 18:07:57 -0500

    initial commit

---
+++ lib/required_methods.rb
@@ -0,0 +1,51 @@
+module RequiredMethods
+  
+  def self.included(base)
+    base.extend ClassMethods
+    base.send(:include, InstanceMethods)
+  end
+  
+  class NoRequiredMethodError < NoMethodError
+  end
+  
+  module ClassMethods
+    
+    def required_class_methods(*args)
+      @@required_class_methods = args
+    end
+    
+    def requires_class_method?(method)
+      @@required_class_methods.include?(method)
+    end
+    
+    def required_instance_methods(*args)
+      @@required_instance_methods = args
+    end
+    
+    def requires_instance_method?(method)
+      @@required_instance_methods.include?(method)
+    end
+    
+    def method_missing(method_sym, *arguments, &block)
+      if requires_class_method?(method_sym)
+        raise NoRequiredMethodError.new("#{name} requires class method #{method_sym}")
+      else
+        super
+      end
+    end
+    
+  end
+  
+  module InstanceMethods
+    
+    def method_missing(method, *arguments, &block)
+      if self.class.requires_instance_method?(method)
+        raise NoRequiredMethodError.new("#{self.class.name} requires instance method #{method}")
+      else
+        super
+      end
+    end
+    
+  end
+  
+end
--- test/test_required_methods.rb
+++ test/test_required_methods.rb
@@ -1,7 +1,67 @@
 require 'helper'
 
-class TestRequiredMethods < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+class Vehicle
+  include RequiredMethods
+  required_class_methods :make, :model
+  required_instance_methods :year, :mileage
+end
+
+class Car < Vehicle
+  
+end
+
+class Truck < Vehicle
+  
+  def self.make
+    "Chevy"
   end
+  
+  def self.model
+    "Blazer"
+  end
+  
+  def year
+    1990
+  end
+  
+  def mileage
+    10000
+  end
+  
+end
+
+class TestRequiredMethods < Test::Unit::TestCase
+  
+  [:make, :model].each do |method|
+    
+    should "raise error if required class methods not defined" do
+      assert_raise(RequiredMethods::NoRequiredMethodError) do
+        Car.send(method)
+      end
+    end
+    
+    should "not raise error if required class methods not defined" do
+      assert_nothing_raised do
+        Truck.send(method)
+      end
+    end
+    
+  end
+  
+  [:year, :mileage].each do |method|
+    
+    should "raise error if required instance methods not defined" do
+      assert_raise(RequiredMethods::NoRequiredMethodError) do
+        Car.new.send(method)
+      end
+    end
+    
+    should "not raise error if required class_methods defined" do
+      assert_nothing_raised do
+        Truck.new.send(method)
+      end
+    end
+    
+  end
+  
 end
